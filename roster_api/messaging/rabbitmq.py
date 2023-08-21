e1dc115cfc7cf7a57567e8b76c8a29213d38e0be
Evan Doyle <evanmdoyle@gmail.com>
2023-08-21 15:44:24 +0200

    Swap from pika to aio-pika for native asyncio support

---
+++ roster_api/messaging/rabbitmq.py
@@ -0,0 +1,3 @@
+# What interface does this need to expose to the rest of the server?
+# publishing messages with arbitrary content to arbitrary topic (naming convention assumed)
+# subscribing to messages with a callback on arbitrary topic (Actor inboxes)
